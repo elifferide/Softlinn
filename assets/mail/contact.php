839f813b4efb55535c60d352388a4a56af46e5e0
Elif F <elff931@gmail.com>
2021-03-10 23:57:51 +0300

    version10.03

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "softlinnusa@gmail.com";
+$address = "info@softlinn.com";
 
 
 // Configuration option.
